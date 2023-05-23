7cb1d17f4c9637fa4b30574ff653d2e8f98dbe4a
Britny Brito <britnybrito@Britnys-MacBook-Air.local>
2023-05-22 18:37:31 -0600

    sin probar

--- FPIngSoft/FiftyFriends/admin.py
+++ FPIngSoft/FiftyFriends/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from FiftyFriends.models import administrador,platillo,c_tipo_platillo
 
-# Register your models here.
+admin.site.register(administrador)
+admin.site.register(c_tipo_platillo)
+admin.site.register(platillo)
